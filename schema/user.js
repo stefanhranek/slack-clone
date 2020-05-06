fa43c13dad79c9bf83354418dcb40552dd8f5efe
stefan hranek <hranekstefan@gmail.com>
2020-05-05 19:55:07 -1000

    Update user schema

--- schema/user.js
+++ schema/user.js
@@ -1,31 +1,30 @@
 // CRUD
 // Create
-// Read 
+// Read
 // Update
 // Delete
 
 export default `
 
-    type User {
-        id: Int!
-        username: String!
-        email: String!
-        teams: [Team!]!
-    }
+  type User {
+    id: Int!
+    username: String!
+    email: String!
+    teams: [Team!]!
+  }
 
-    type Query {
-        getUser(id: Int!): User!
-        allUsers: [User!]!
-    }
+  type Query {
+    getUser(id: Int!): User!
+    allUsers: [User!]!
+  }
 
-    type RegisterResponse {
-        ok: Boolean!
-        user: User
-        errors: [Error!]
-    }
-
-    type Mutation {
-        register(username: String!, email: String!, password: String!): RegisterResponse!
-    }
-
+  type RegisterResponse {
+    ok: Boolean!
+    user: User
+    errors: [Error!]
+  }
+  
+  type Mutation {
+    register(username: String!, email: String!, password: String!): RegisterResponse!
+  }
 `;
