f5965dec0258f220e5d28c1dc51802b41a9a2a23
stefan hranek <hranekstefan@gmail.com>
2020-05-27 15:53:13 -1000

    Finally fixed registration errors w/ 'Sequalize'

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,20 +1,20 @@
+import bcrypt from "bcrypt";
+import _ from "lodash";
 
-import bcrypt from 'bcrypt';
-import _ from 'lodash';
-
-import { tryLogin } from '../auth';
+import { tryLogin } from "../auth";
 
 const formatErrors = (e, models) => {
-  if (e instanceof models.sequelize.ValidationError) {
+  if (e instanceof models.Sequelize.ValidationError) {
     //  _.pick({a: 1, b: 2}, 'a') => {a: 1}
-    return e.errors.map(x => _.pick(x, ['path', 'message']));
+    return e.errors.map((x) => _.pick(x, ["path", "message"]));
   }
-  return [{ path: 'name', message: 'something went wrong' }];
+  return [{ path: "name", message: "something went wrong" }];
 };
 
 export default {
   Query: {
-    getUser: (parent, { id }, { models }) => models.User.findOne({ where: { id } }),
+    getUser: (parent, { id }, { models }) =>
+      models.User.findOne({ where: { id } }),
     allUsers: (parent, args, { models }) => models.User.findAll(),
   },
   Mutation: {
