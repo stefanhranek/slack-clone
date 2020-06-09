467ad633230ca2239c79580dc64d3fd06db01f2e
stefan hranek <hranekstefan@gmail.com>
2020-06-08 23:34:56 -1100

    create team script done - teams store owner id

--- auth.js
+++ auth.js
@@ -26,8 +26,8 @@ export const createTokens = async (user, secret, secret2) => {
   return [createToken, createRefreshToken];
 };
 
-export const refreshTokens = async (token, refreshToken, models, SECRET) => {
-  let userId = -1;
+export const refreshTokens = async (token, refreshToken, models, SECRET, SECRET2) => {
+  let userId = 0;
   try {
     const {
       user: { id },
@@ -47,8 +47,10 @@ export const refreshTokens = async (token, refreshToken, models, SECRET) => {
     return {};
   }
 
+  const refreshSecret = user.password + SECRET;
+
   try {
-    jwt.verify(refreshToken, user.refreshSecret);
+    jwt.verify(refreshToken, refreshSecret);
   } catch (err) {
     return {};
   }
@@ -56,7 +58,7 @@ export const refreshTokens = async (token, refreshToken, models, SECRET) => {
   const [newToken, newRefreshToken] = await createTokens(
     user,
     SECRET,
-    user.refreshSecret
+    refreshSecret
   );
   return {
     token: newToken,
